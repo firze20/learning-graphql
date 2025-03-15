fd499d0b8a4b8d7e67fa8885ff7738d126be3ac0
Domingues <domingues.fjp@hotmail.com>
2025-03-15 16:18:15 +0000

    added sample graphql

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const { createHandler } = require("graphql-http/lib/use/express");
+const { buildSchema } = require("graphql");
+const { ruruHTML } = require("ruru/server");
 const express = require("express");
-const graphqlHTTP = require("graphql-http");
 
 const app = express();
 
@@ -8,29 +10,48 @@ const { User } = require("./models/models");
 
 const PORT = 4000;
 
-// Simple route to confirm the server is running
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
-
-app.get("/users/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    // Find the user with the given ID
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
-    // Send the user back as a response
-    res.send(user);
-  } catch (err) {
-    // Log the error to the console
-    console.error(err);
-    res.status(500).send("An error occurred while trying to find the user");
+// Construct our GraphQL schema, using the GraphQL schema language
+// This schema has two queries: user and users
+// The user query takes an id argument and returns a single User
+// The users query returns an array of all User objects
+const schema = buildSchema(`
+  type User {
+    _id: String
+    username: String
   }
+  
+  type Query {
+    user(id: String): User 
+    users: [User]
+  }
+`);
+
+// Define resolvers
+const rootValue = {
+  // Resolver for single user
+  async user({ id }) {
+    return await User.findById(id);
+  },
+  // Resolver for all users
+  async users() {
+    return await User.find();
+  }
+};
+
+//Serve the GraphiQL IDE
+app.get('/', (req, res) => {
+  res.type('html');
+  res.end(ruruHTML({ endpoint: '/graphql' }));
 });
 
-// Start the server
+// Start up a GraphQL endpoint listenning at /graphql
+app.all("/graphql", createHandler({ 
+  schema, 
+  rootValue,
+}));
+
+
+// Start the application, listenning on port 4000
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`GraphQL is running on http://localhost:${PORT}/graphql`);
 });
